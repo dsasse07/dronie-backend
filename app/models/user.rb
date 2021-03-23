54c06334ee395599f183029ab3f6992a42b20b57
Daniel Sasse <dsasse07@gmail.com>
2021-03-23 18:02:03 -0400

    MVP dev merged

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :likes, dependent: :destroy
-  has_many :liked_posts, through: :likes, source: :post
+  has_many :liked_posts, through: :likes, source: :post 
 
   has_many :comments, dependent: :destroy
   has_many :commented_posts, through: :comments, source: :post
